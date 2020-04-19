6023e102391a03f3ff07fe349d26b1ba7f992eec
Tobias Anker <tobias.anker@kitsunemimi.moe>
2020-04-19 21:25:28 +0200

    cleanup in common files

--- src/common/typedefs.h
+++ src/common/typedefs.h
@@ -12,13 +12,12 @@
 
 #include <chrono>
 
-typedef std::chrono::microseconds  chronoMicroSec;
-typedef std::chrono::nanoseconds  chronoNanoSec;
-typedef std::chrono::seconds  chronoSec;
+typedef std::chrono::microseconds chronoMicroSec;
+typedef std::chrono::nanoseconds chronoNanoSec;
+typedef std::chrono::seconds chronoSec;
 typedef std::chrono::high_resolution_clock::time_point chronoTimePoint;
 typedef std::chrono::high_resolution_clock chronoClock;
 
 typedef uint32_t BrickID;
-typedef uint8_t BYTE;
 
 #endif // TYPEDEFS_H
