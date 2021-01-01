40364f60cb87e8afad1714a4c71e707648749de3
Tobias Anker <tobias.anker@kitsunemimi.moe>
2021-01-01 22:29:01 +0100

    update comments in new blossom-initializing

--- src/initializing/blossom_initializing.h
+++ src/initializing/blossom_initializing.h
@@ -1,3 +1,25 @@
+/**
+ * @file        blossom_initializing.h
+ *
+ * @author      Tobias Anker <tobias.anker@kitsunemimi.moe>
+ *
+ * @copyright   Apache License Version 2.0
+ *
+ *      Copyright 2019 Tobias Anker
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ */
+
 #ifndef BLOSSOM_INITIALIZING_H
 #define BLOSSOM_INITIALIZING_H
 
@@ -21,7 +43,7 @@
 using Kitsunemimi::Sakura::SakuraLangInterface;
 
 /**
- * @brief initSpecialBlossoms
+ * @brief init special blossoms
  */
 void
 initSpecialBlossoms()
@@ -37,7 +59,7 @@ initSpecialBlossoms()
 }
 
 /**
- * @brief initRegisterBlossoms
+ * @brief init register blossoms
  */
 void
 initRegisterBlossoms()
@@ -49,7 +71,7 @@ initRegisterBlossoms()
 }
 
 /**
- * @brief initMetadataBlossoms
+ * @brief init metadata blossoms
  */
 void
 initMetadataBlossoms()
@@ -60,7 +82,7 @@ initMetadataBlossoms()
 }
 
 /**
- * @brief initGlobalValuesBlossoms
+ * @brief init global_values blossoms
  */
 void
 initGlobalValuesBlossoms()
@@ -71,7 +93,7 @@ initGlobalValuesBlossoms()
 }
 
 /**
- * @brief initSnapshotBlossoms
+ * @brief init snapshot blossoms
  */
 void
 initSnapshotBlossoms()
@@ -81,7 +103,7 @@ initSnapshotBlossoms()
     assert(interface->addBlossom("snapshot", "obj", new SnapshotBlossom()));
 }
 /**
- * @brief initBlossoms
+ * @brief init all blossoms
  */
 void
 initBlossoms()
