36e1d379083a30449c9abc5381720b1938dd77e8
Tobias Anker <tobias.anker@kitsunemimi.moe>
2021-09-12 14:39:50 +0200

    add functionality to generate uuids

--- src/common/functions.h
+++ src/common/functions.h
@@ -25,5 +25,35 @@
 
 #include <stdint.h>
 #include "structs.h"
+#include <uuid/uuid.h>
+
+struct kuuid
+{
+    char uuid[UUID_STR_LEN];
+    uint8_t padding[3];
+
+    const std::string toString() {
+        return std::string(uuid);
+    }
+
+    // total size: 40 Bytes
+};
+
+/**
+ * @brief generate a new uuid with external library
+ *
+ * @return new uuid
+ */
+inline const kuuid
+generateUuid()
+{
+    uuid_t binaryUuid;
+    kuuid result;
+
+    uuid_generate_random(binaryUuid);
+    uuid_unparse_lower(binaryUuid, result.uuid);
+
+    return result;
+}
 
 #endif // KYOUKOMIND_FUNCTIONS_H
